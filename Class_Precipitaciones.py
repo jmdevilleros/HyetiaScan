426306add0f102025facf10c5471a2c05c41fd23
Juan de Villeros <41244769+jmdevilleros@users.noreply.github.com>
2023-10-30 16:47:11 -0500

    Progreso de desarrollo
    Intento aparentemente fallido de manejar preprocesamiento de datos en pagina independiente de la selecciond e columnas. Todo queda demasiado interdependiente.

--- Class_Precipitaciones.py
+++ Class_Precipitaciones.py
@@ -23,15 +23,16 @@ class Precipitaciones:
     def __init__(self):
         self.archivo_io  = None # Objeto BytesIO para lectura física de archivo
         self.df_lecturas = None # Dataframe que almacena las lecturas de precipitacion
-        self.df_eventos  = pd.DataFrame() # Luego preprocesamiento de lecturas
+        self.df_eventos  = None # Luego de preprocesamiento de lecturas
 
-        self.col_fechahora     = None
-        self.col_precipitacion = None
+        self.col_fechahora        = None
+        self.col_precipitacion    = None
+        self.intervalo_mediciones = None
 
     # -----------------------------------------------------------------------------------------
     def obtener_lecturas(self):
         # Reiniciar variables dependientes del contenido
-        self.df_eventos        = pd.DataFrame()
+        self.df_eventos        = None
         self.col_fechahora     = None
         self.col_precipitacion = None
 
@@ -81,12 +82,111 @@ class Precipitaciones:
     def asignar_columnas_seleccionadas(self, col_fechahora, col_precipitacion):
         fechashoras = self._obtener_columna_fechahora(col_fechahora)
         precipitaciones = self._obtener_columna_precipitacion(col_precipitacion)
+
         if (fechashoras is None) | (precipitaciones is None):
             return False
-        else:
-            self.col_fechahora = col_fechahora
-            self.col_precipitacion = col_precipitacion
-            self.df_eventos = pd.DataFrame(
-                {col_fechahora : fechashoras, col_precipitacion : precipitaciones}
-            )
+
+        self.col_fechahora = col_fechahora
+        self.col_precipitacion = col_precipitacion
+        self.df_eventos = pd.DataFrame(
+            {col_fechahora : fechashoras, col_precipitacion : precipitaciones}
+        )
+        self.intervalo_mediciones = 0
+
+        return True
+
+    # -----------------------------------------------------------------------------------------
+    def detectar_intervalo_mediciones(self):
+        if self.intervalo_mediciones > 0:
+            # Ya detectado, terminar
             return True
+        
+        es_intervalo_detectable = \
+            (isinstance(self.df_eventos, pd.DataFrame)) & \
+            (self.df_eventos.shape[0] > 1)              & \
+            (self.col_fechahora is not None)
+
+        if not es_intervalo_detectable:
+            return False
+        
+        t0 = self.df_eventos.iloc[0][self.col_fechahora]
+        t1 = self.df_eventos.iloc[1][self.col_fechahora]
+        self.intervalo_mediciones = (t1 - t0).seconds / 60
+
+        if self.intervalo_mediciones < 1:
+            return False
+
+        return True
+
+    # -----------------------------------------------------------------------------------------
+    def detectar_vacios(self):
+        # Valores de precipitacion menor a cero son errores de medición, eliminarlos 
+        # para dejar que se procesen en las lineas siguientes como lagunas de datos
+        self.df_eventos = self.df_eventos[self.df_eventos[self.col_precipitacion] >= 0]
+
+        delta = pd.Timedelta(minutes=self.intervalo_mediciones)
+        vacios = self.df_eventos[self.col_fechahora].diff() > delta
+        vacios_indices = self.df_eventos.loc[vacios, self.col_fechahora]
+        
+        num_vacios = len(vacios_indices)
+        if num_vacios <= 0:
+            return 0, None
+        
+        inicio_vacios = self.df_eventos.loc[
+            vacios.shift(-1, fill_value=False), self.col_fechahora
+        ].tolist()
+        termina_vacios = self.df_eventos.loc[
+            vacios, self.col_fechahora
+        ].tolist()
+        df_vacios = pd.DataFrame({
+            'inicia_vacio'  : inicio_vacios, 
+            'termina_vacio' : termina_vacios,
+        })
+        df_vacios['duracion_vacio'] = \
+            (df_vacios['termina_vacio'] - df_vacios['inicia_vacio']).dt.total_seconds() // 60
+
+        return num_vacios, df_vacios
+
+    # -----------------------------------------------------------------------------------------
+    def rellenar_vacios(self):
+        rango_completo = pd.date_range(
+            start=self.df_eventos[self.col_fechahora].min(), 
+            end=self.df_eventos[self.col_fechahora].max(), 
+            freq=f'{self.intervalo_mediciones}T'
+        )
+        timestamps_faltantes = rango_completo.difference(self.df_eventos[self.col_fechahora])
+        df_faltantes = pd.DataFrame(timestamps_faltantes, columns=[self.col_fechahora])
+
+        df_faltantes[self.col_precipitacion] = 0
+        self.df_eventos = \
+            pd.concat([self.df_eventos, df_faltantes]).sort_values(by=self.col_fechahora)
+
+        self.df_eventos.reset_index(drop=True, inplace=True)
+
+        return
+    
+    # -----------------------------------------------------------------------------------------
+    def calcular_eventos_precipitacion(self):
+        # Crear una columna adicional para marcar los cambios de 0 a otro valor y viceversa
+        self.df_eventos['cambio'] = \
+            (self.df_eventos[self.col_precipitacion] != 0) != \
+            (self.df_eventos[self.col_precipitacion] != 0).shift(1)
+        self.df_eventos['grupo'] = self.df_eventos['cambio'].cumsum()
+
+        # Realizar la agregación basada en el grupo
+        self.df_eventos = self.df_eventos.groupby('grupo').agg(
+            inicia                  = (self.col_fechahora, 'first'),
+            termina                 = (self.col_fechahora, 'last'),
+            conteo                  = (self.col_precipitacion, 'count'),
+            precipitacion_acumulada = (self.col_precipitacion, 'sum'),
+            mediciones              = (self.col_precipitacion, lambda p: p.tolist()),
+        ).reset_index(drop=True)
+
+        # Agregar coiumna de duracion
+        self.df_eventos['duracion'] = \
+            (self.df_eventos['termina'] - self.df_eventos['inicia']).dt.total_seconds() // 60 \
+            + self.intervalo_mediciones
+
+    
+    
+    
--- pages/3-Examinar eventos.py
+++ pages/3-Examinar eventos.py
@@ -34,11 +34,44 @@ apcfg.configurar_pagina('Examinar eventos. :umbrella_with_rain_drops:')
 
 # Verificar que haya datos y columnas seleccionadas
 if datos.df_lecturas is None:
-    st.error(f'No hay eventos para visualizar.')
+    st.error(f'No hay lecturas para procesar.')
     st.stop()
 if (datos.col_fechahora is None) | (datos.col_precipitacion is None):
     st.error('Debe seleccionar columnas de **fecha/hora** y de **precipítación**.')
     st.stop()
 
-# Procesar
+# Detectar y mostrar intervalo de mediciones
+if not datos.detectar_intervalo_mediciones():
+    st.write('Error detectando intervalo entre mediciones.')
+    st.stop()
+
+# Mostrar intervalo detectado
+st.caption(f'Intervalo detectado entre mediciones: {datos.intervalo_mediciones} minutos')
+
+# Verificar si hay vacíos o "lagunas" en las mediciones
+num_vacios, df_vacios = datos.detectar_vacios()
+if num_vacios > 0:
+    st.warning(f'Atención: {num_vacios} vacios detectados en la serie de mediciones.')
+    c1, c2 = st.columns(2)
+    with c1:
+        rellenar = st.button('Rellenar con ceros?')
+    with c2:
+        ver_vacios = st.toggle('Ver detalle de vacíos', disabled=num_vacios <= 0)
+
+    if ver_vacios:
+        st.dataframe(df_vacios)
+        recibidos = datos.df_lecturas.shape[0]
+        st.caption('Faltantes:')
+
+    if (rellenar):
+        st.toast('Rellenando con ceros las mediciones faltantes...')
+        datos.rellenar_vacios()
+        st.rerun()
+    else:
+        st.error('No se pueden procesar eventos con vacíos en las lecturas.')
+        st.stop()
+
+# Calcular eventos
+datos.calcular_eventos_precipitacion()
 st.dataframe(datos.df_eventos)
+
